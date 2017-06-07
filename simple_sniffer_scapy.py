44e1616e5515877094e94420c620adf19c1bbf6e
Nishan8583 <nishanmaharjan17@gmail.com>
2017-06-07 13:29:13 +0545

    Update simple_sniffer_scapy.py

--- simple_sniffer_scapy.py
+++ simple_sniffer_scapy.py
@@ -1,11 +1,31 @@
 # Run tis code as root, scapy needs to be installed
 #!/usr/bin/python
 from scapy.all import *
-
+limit = 2500
+var = 0
+ip_list = {}
 def display(pkt):
         pkt.show()
 
-sniff(prn = display)
+def filter(pkt):
+        if pkt.getlayer(IP).src == "127.0.0.1":
+                print "local IP"
+
+def check_DDOS(pkt):
+        source = pkt.getlayer(IP).src
+        if source not in ip_list:
+                ip_list[source] = 1
+        else:
+                ip_list[source] = ip_list[source] + 1
+
+        for source,number in ip_list.items():
+                if number > limit:
+                        print "[*] WARNING TOO MANY PACKET FROM {} [*]".format(source)
+                        return
+                else:
+                        pkt.show()
+sniff(prn = check_DDOS)
+
 
 # In prn different type of function can be passed, that acts accordingly
 
